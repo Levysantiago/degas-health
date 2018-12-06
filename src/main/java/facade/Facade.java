5888c1b8059c98ff7d8a0c873fe3cc20277af7b5
Levysantiago <levyssantiago@gmail.com>
2018-12-06 14:57:29 -0300

    Classe Facade do pacote 'facade' documentada

--- src/main/java/facade/Facade.java
+++ src/main/java/facade/Facade.java
@@ -7,17 +7,15 @@ import cadastro.CadastroEspecialidade;
 import cadastro.CadastroMaterial;
 import cadastro.CadastroMedico;
 import cadastro.CadastroProcedimento;
-import modelo.Auxiliar;
-import modelo.Enfermeiro;
-import modelo.Medico;
+import modelo.Equipamento;
+import modelo.Material;
+import modelo.Procedimento;
+import modelo.Tombo;
 
 /**
- * This class works as a middleware. It'll get all the requests and redirect to the right class method.
- * It defines three attributes: cadastroMedico, cadastroEnfermeiro and cadastroAuxiliar, they are
- * specialist objects to create, read and update {@link Medico}, {@link Enfermeiro} and {@link Auxiliar}
- * objects.
+ * Esta classe trabalha como um middleware. Ela irá receber todos os requests e redirecionar para
+ * o método de classe correto.
  */
-
 public class Facade {
 
 	private CadastroMedico cadastroMedico;
@@ -29,7 +27,9 @@ public class Facade {
 	private static CadastroMaterial cadastroMaterial;
 	
 	/**
-	 * This method is Facade Constructor.
+	 * Este é o contrutor da {@link Facade}. Ele instancia as classes de cadastro {@link CadastroMedico},
+	 * {@link CadastroEnfermeiro}, {@link CadastroAuxiliar}, {@link CadastroProcedimento},
+	 * {@link CadastroEspecialidade}, {@link CadastroEquipamento} e {@link CadastroMaterial}.
 	 */
 	public Facade() {
 		 cadastroMedico = new CadastroMedico();
@@ -42,179 +42,294 @@ public class Facade {
 	}
 
 	/**
-	 * This method registers a new doctor by redirecting to {@link CadastroMedico}.
-	 * @param nome - The doctor's name
-	 * @param sex - The doctor's gender
-	 * @param crm - The doctor's CRM
-	 * @param nat - The doctor's nationality
-	 * @param bthDate - The doctor's birth date
-	 * @param admDate - The doctor's admission date
-	 * @param gradDate - The doctor's graduation date
-	 * @return A message indicating an error or success to create a new doctor
+	 * Este método registra um novo médico redirecionando para {@link CadastroMedico}.
+	 * @param nome - O nome do médico
+	 * @param sex - Sexo do médico
+	 * @param crm - CRM do médico
+	 * @param nat - Nationalidade
+	 * @param bthDate - Data de nascimento
+	 * @param admDate - Data de admissão
+	 * @param gradDate - Data de formatura
+	 * @return Uma mensagem informando o sucesso ou não da inserção do médico no banco
 	 */
 	public String novoMedico(String nome, String sex, String crm, String nat, String bthDate, String admDate, String gradDate) {
 		return cadastroMedico.novo(nome, sex, crm, nat, bthDate, admDate, gradDate);
 	}
 	
 	/**
-	 * This method finds a doctor by redirecting to {@link CadastroMedico}.
-	 * @param atributo - The name or CRM of a doctor
-	 * @return A string containing all information of the doctor found.<br>
-	 * 		If none doctor was found, the method returns ""
+	 * Este método encontra um médico a partir do atributo recebido, redirecionando para {@link CadastroMedico}.
+	 * @param atributo - O nome ou CRM do médico
+	 * @return Uma String contendo toda a informação do médico encontrado
+	 * 		Se o médico não foi encontrado, o método retorna "" (Uma String vazia)
 	 */
 	public String encontraMedico(String atributo) {
 		return cadastroMedico.encontra(atributo);
 	}
 	
 	/**
-	 * This method modifies an existent doctor by redirecting to {@link CadastroMedico}.
-	 * @param crm - The doctor's CRM
-	 * @param atributo - A doctor's attribute (Nome, CRM, Sexo...)
-	 * @param valor - The value to subscribe the old attribute value
-	 * @return A message indicating an error or a success.
+	 * Este método modifica um médico existente redirecionando a requisição para {@link CadastroMedico}.
+	 * @param crm - O CRM do médico
+	 * @param atributo - O campo o qual o dado será alterado (Nome, CRM, Sexo...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
 	 */
 	public String alteraMedico(String crm, String atributo, String valor) {
 		return cadastroMedico.altera(crm, atributo, valor);
 	}
 	
 	/**
-	 * This method registers a new nurse by redirecting to {@link CadastroEnfermeiro}.
-	 * @param nome - The nurse's name
-	 * @param coren - The nurse's COREN
-	 * @param sex - The nurse's gender
-	 * @param nat - The nurse's nationality
-	 * @param bthDate - The nurse's birth date
-	 * @param gradDate - The nurse's graduation date
-	 * @param admDate - The nurse's admission date
+	 * Este método registra um novo enfermeiro redirecionando a requisição para {@link CadastroEnfermeiro}.
+	 * @param nome - O nome do enfermeiro
+	 * @param sex - Sexo do enfermeiro
+	 * @param coren - Coren do enfermeiro
+	 * @param nat - Nationalidade
+	 * @param bthDate - Data de nascimento
+	 * @param admDate - Data de admissão
+	 * @param gradDate - Data de formatura
 	 * 
-	 * @return A message indicating an error or success to create a new nurse
+	 * @return Uma mensagem informando o sucesso ou não da inserção do enfermeiro no banco
 	 */
 	public String novoEnfermeiro(String nome, String sex, String coren, String nat, String bthDate, String admDate, String gradDate) {
 		return cadastroEnfermeiro.novo(nome, sex, coren, nat, bthDate, admDate, gradDate);
 	}
 	
 	/**
-	 * This method finds a nurse in nurses list by redirecting to {@link CadastroEnfermeiro}.
-	 * @param atributo - The name or COREN of a nurse
-	 * @return A string containing all information of the nurse found.<br>
-	 * 		If the nurse wasn't found, the method returns ""
+	 * Este método encontra um enfermeiro no banco redirecionando a requisição para {@link CadastroEnfermeiro}.
+	 * @param atributo - O nome ou Coren do enfermeiro
+	 * @return Uma String contendo toda a informação do enfermeiro encontrado
+	 * 		Se o enfermeiro não foi encontrado, o método retorna "" (Uma String vazia)
 	 */
 	public String encontraEnfermeiro(String atributo) {
 		return cadastroEnfermeiro.encontra(atributo);
 	}
 	
 	/**
-	 * This method modify an existent nurse by redirecting to {@link CadastroEnfermeiro}.
-	 * @param coren - The nurse's COREN
-	 * @param atributo - A nurse's attribute (Nome, COREN, Sexo...)
-	 * @param valor - The value to subscribe the old attribute value
-	 * @return A message indicating an error or a success.
+	 * Este método modifica um enfermeiro existente redirecionando a requisição para {@link CadastroEnfermeiro}.
+	 * @param crm - O Coren do enfermeiro
+	 * @param atributo - O campo o qual o dado será alterado (Nome, Coren, Sexo...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
 	 */
 	public String alteraEnfermeiro(String coren, String atributo, String valor) {
 		return cadastroEnfermeiro.altera(coren, atributo, valor);
 	}
 	
 	/**
-	 * This method registers a new assistant by redirecting to {@link CadastroAuxiliar}.
-	 * @param nome - The assistant's name
-	 * @param coren - The assistant's COREN
-	 * @param sex - The assistant's gender
-	 * @param nat - The assistant's nationality
-	 * @param bthDate - The assistant's birth date
-	 * @param gradDate - The assistant's graduation date
-	 * @param admDate - The assistant's admission date
+	 * Este método registra um novo auxiliar redirecionando a requisição para {@link CadastroAuxiliar}.
+	 * @param nome - O nome do auxiliar
+	 * @param sex - Sexo do auxiliar
+	 * @param coren - Coren do auxiliar
+	 * @param nat - Nationalidade
+	 * @param bthDate - Data de nascimento
+	 * @param admDate - Data de admissão
+	 * @param gradDate - Data de formatura
 	 * 
-	 * @return A message indicating an error or success to create a new assistant
+	 * @return Uma mensagem informando o sucesso ou não da inserção do auxiliar no banco
 	 */
 	public String novoAuxiliar(String nome, String sex, String coren, String nat, String bthDate, String admDate, String gradDate) {
 		return cadastroAuxiliar.novo(nome, sex, coren, nat, bthDate, admDate, gradDate);
 	}
 	
 	/**
-	 * This method finds an assistant in assistants list by redirecting to {@link CadastroAuxiliar}.
-	 * @param atributo - The name or COREN of an assistant
-	 * @return A string containing all information of the assistant found.<br>
-	 * 		If the assistant wasn't found, the method returns ""
+	 * Este método encontra um auxiliar no banco redirecionando a requisição para {@link CadastroAuxiliar}.
+	 * @param atributo - O nome ou Coren do auxiliar
+	 * @return Uma String contendo toda a informação do auxiliar encontrado
+	 * 		Se o auxiliar não foi encontrado, o método retorna "" (Uma String vazia)
 	 */
 	public String encontraAuxiliar(String atributo) {
 		return cadastroAuxiliar.encontra(atributo);
 	}
 	
 	/**
-	 * This method modify an existent assistant by redirecting to {@link CadastroAuxiliar}.
-	 * @param coren - The assistant's COREN
-	 * @param atributo - An assistant's attribute (Nome, COREN, Sexo...)
-	 * @param valor - The value to subscribe the old attribute value
-	 * @return A message indicating an error or a success.
+	 * Este método modifica um auxiliar existente redirecionando a requisição para {@link CadastroAuxiliar}.
+	 * @param crm - O Coren do auxiliar
+	 * @param atributo - O campo o qual o dado será alterado (Nome, Coren, Sexo...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
 	 */
 	public String alteraAuxiliar(String crm, String atributo, String valor) {
 		return cadastroAuxiliar.altera(crm, atributo, valor);
 	}
 	
+	/**
+	 * Este método registra um novo procedimento redirecionando a requisição para {@link CadastroProcedimento}.
+	 * @param codigo - O código do procedimento
+	 * @param descricao - A descrição do mesmo
+	 * @param custo - O custo do procedimento
+	 * 
+	 * @return Uma mensagem informando o sucesso ou não da inserção do procedimento no banco
+	 */
 	public static String novoProcedimento(String codigo, String atributo, String valor) {
 		return cadastroProcedimento.novo(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método modifica um procedimento existente redirecionando a requisição para {@link CadastroProcedimento}.
+	 * @param codigo - O Código do procedimento
+	 * @param atributo - O campo o qual o dado será alterado (Descrição, Custo...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
+	 */
 	public static String alteraProcedimento(String codigo, String atributo, String valor) {
 		return cadastroProcedimento.altera(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método encontra um procedimento no banco redirecionando a requisição para {@link CadastroProcedimento}.
+	 * @param atributo - A descrição ou custo do procedimento
+	 * @return Uma String contendo toda a informação do procedimento encontrado
+	 * 		Se o enfermeiro não foi encontrado, o método retorna "" (Uma String vazia)
+	 */
 	public static String encontraProcedimento(String atributo) {
 		return cadastroProcedimento.encontra(atributo);
 	}
 	
+	/**
+	 * Este método cadastra um material em um procedimento redirecionando a requisição para {@link CadastroProcedimento}.
+	 * @param codProcedimento - O código do procedimento (ver {@link Procedimento})
+	 * @param codMaterial - O código do material (ver {@link Material})
+	 * @return Uma String informando o sucesso ou não na inserção do material.
+	 */
 	public static String materialProcedimento(String codProcedimento, String codMaterial) {
 		return cadastroProcedimento.addMaterial(codProcedimento, codMaterial);
 	}
 	
+	/**
+	 * Este método cadastra um equipamento em um procedimento redirecionando a requisição para {@link CadastroProcedimento}.
+	 * @param codProcedimento - O código do procedimento (ver {@link Procedimento})
+	 * @param codEquipamento - O código do equipamento (ver {@link Equipamento})
+	 * @return Uma String informando o sucesso ou não na inserção do equipamento.
+	 */
 	public static String equipamentoProcedimento(String codProcedimento, String codEquipamento) {
 		return cadastroProcedimento.addEquipamento(codProcedimento, codEquipamento);
 	}
 	
+	/**
+	 * Este método recupera a descrição de materiais cadastrados em um procedimento redirecionando a 
+	 * requisição para {@link CadastroProcedimento}.
+	 * @param codProcedimento - O código do procedimento (ver {@link Procedimento})
+	 * @return Caso existam materiais, é retornada uma String contendo as descrições do 
+	 * mesmos. Caso contrário é retornado "" (uma String vazia).
+	 */
 	public String listMateriaisProcedimento(String codProcedimento) {
 		return cadastroProcedimento.listMateriais(codProcedimento);
 	}
 	
+	/**
+	 * Este método recupera a descrição de equipamentos cadastrados em um procedimento redirecionando a 
+	 * requisição para {@link CadastroProcedimento}.
+	 * @param codProcedimento - O código do procedimento (ver {@link Procedimento})
+	 * @return Caso existam equipamentos, é retornada uma String contendo as descrições do 
+	 * mesmos. Caso contrário é retornado "" (uma String vazia).
+	 */
 	public String listEquipamentosProcedimento(String codProcedimento) {
 		return cadastroProcedimento.listEquipamentos(codProcedimento);
 	}
 	
+	/**
+	 * Este método registra uma nova especialidade redirecionando a requisição para {@link CadastroEspecialidade}.
+	 * @param codigo - O código da especialidade
+	 * @param descricao - A descrição desta
+	 * 
+	 * @return Uma mensagem informando o sucesso ou não da inserção da especialidade no banco
+	 */
 	public static String novaEspecialidade(String codigo, String atributo) {
 		return cadastroEspecialidade.novo(codigo, atributo);
 	}
 	
+	/**
+	 * Este método modifica uma especialidade existente redirecionando a requisição para {@link CadastroEspecialidade}.
+	 * @param codigo - O codigo da especialidade
+	 * @param atributo - O campo o qual o dado será alterado (Descrição...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
+	 */
 	public static String alteraEspecialidade(String codigo, String atributo, String valor) {
 		return cadastroEspecialidade.altera(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método encontra uma especialidade no banco redirecionando a requisição para {@link CadastroEspecialidade}.
+	 * @param atributo - A descrição da especialidade
+	 * @return Uma String contendo toda a informação da especialidade encontrada
+	 * 		Se a especialidade não foi encontrada, o método retorna "" (Uma String vazia)
+	 */
 	public static String encontraEspecialidade(String atributo) {
 		return cadastroEspecialidade.encontra(atributo);
 	}
 	
+	/**
+	 * Este método registra um novo equipamento redirecionando a requisição para {@link CadastroEquipamento}.
+	 * @param codigo - O código do equipamento
+	 * @param descricao - A descrição do mesmo
+	 * @param valor - O preço do equipamento
+	 * 
+	 * @return Uma mensagem informando o sucesso ou não da inserção do equipamento no banco
+	 */
 	public static String novoEquipamento(String codigo, String atributo, String valor) {
 		return cadastroEquipamento.novo(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método modifica um equipamento existente redirecionando a requisição para {@link CadastroEquipamento}.
+	 * @param codigo - O Codigo do equipamento
+	 * @param atributo - O campo o qual o dado será alterado (Descrição, Valor...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
+	 */
 	public static String alteraEquipamento(String codigo, String atributo, String valor) {
 		return cadastroEquipamento.altera(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método encontra um equipamento no banco redirecionando a requisição para {@link CadastroEquipamento}.
+	 * @param atributo - A descrição ou Valor do equipamento
+	 * @return Uma String contendo toda a informação do equipamento encontrado
+	 * 		Se o equipamento não foi encontrado, o método retorna "" (Uma String vazia)
+	 */
 	public static String encontraEquipamento(String atributo) {
 		return cadastroEquipamento.encontra(atributo);
 	}
 	
+	/**
+	 * Este método cadastra um novo tombo em um equipamento redirecionando a requisição para {@link CadastroEquipamento}.
+	 * @param codigoEquipamento - O código do equipamento (ver {@link Equipamento})
+	 * @param codigoTombo - O código do tombo (ver {@link Tombo})
+	 * @return Uma String informando o sucesso ou não do cadastro do tombo do equipamento.
+	 */
 	public static String tombaEquipamento(String codEquipamento, String codTombo) {
 		return cadastroEquipamento.tombaEquipamento(codEquipamento, codTombo);
 	}
 	
+	/**
+	 * Este método registra um novo material redirecionando a requisição para {@link CadastroMaterial}.
+	 * @param codigo - O código do material
+	 * @param descricao - A descrição do mesmo
+	 * @param valor - O preço do material
+	 * 
+	 * @return Uma mensagem informando o sucesso ou não da inserção do material no banco
+	 */
 	public static String novoMaterial(String codigo, String atributo, String valor) {
 		return cadastroMaterial.novo(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método modifica um material existente redirecionando a requisição para {@link CadastroMaterial}.
+	 * @param codigo - O Código do material
+	 * @param atributo - O campo o qual o dado será alterado (Descrição, Valor...)
+	 * @param valor - O novo dado a sobrescrever o dado antigo
+	 * @return Uma mensagem indicando um erro ou sucesso ao realizar a alteração
+	 */
 	public static String alteraMaterial(String codigo, String atributo, String valor) {
 		return cadastroMaterial.altera(codigo, atributo, valor);
 	}
 	
+	/**
+	 * Este método encontra um material no banco redirecionando a requisição para {@link CadastroMaterial}.
+	 * @param atributo - A descrição ou valor do material
+	 * @return Uma String contendo toda a informação do material encontrado
+	 * 		Se o material não foi encontrado, o método retorna "" (Uma String vazia)
+	 */
 	public static String encontraMaterial(String atributo) {
 		return cadastroMaterial.encontra(atributo);
 	}
